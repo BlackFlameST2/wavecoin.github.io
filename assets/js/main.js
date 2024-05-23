a2a674f5270403d05c650f9a71c3e31f75b6915a
BlackFlameST <blackflame.osv@mail.ru>
2024-05-23 15:16:24 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,85 +5,83 @@ const h1 = body.querySelector('h1');
 let coins = localStorage.getItem('coins');
 let total = localStorage.getItem('total');
 let power = localStorage.getItem('power');
-let count = localStorage.getItem('count')
-let tg = window.Telegram.WebApp;
+let count = localStorage.getItem('count');
+let tg;
 
-
-if(tg != null) {
-    body.querySelector('login').textContent= tg.initDataUnsafe.user.username;
+try {
+    tg = window.Telegram.WebApp;
+} catch (e) {
+    tg = null;
 }
-if(coins == null){
-    localStorage.setItem('coins' , '0');
+
+if (tg != null) {
+    body.querySelector('.login .name').textContent = tg.initDataUnsafe.user.username;
+}
+
+if (coins == null) {
+    localStorage.setItem('coins', '0');
     h1.textContent = '0';
-}else{
+} else {
     h1.textContent = Number(coins).toLocaleString();
 }
 
-if(total == null){
-    localStorage.setItem('total' , '500')
+if (total == null) {
+    localStorage.setItem('total', '500');
     body.querySelector('#total').textContent = '/500';
-}else {
+} else {
     body.querySelector('#total').textContent = `/${total}`;
 }
 
-
-if(power == null){
-    localStorage.setItem('power' , '500');
+if (power == null) {
+    localStorage.setItem('power', '500');
     body.querySelector('#power').textContent = '500';
-}else{
+} else {
     body.querySelector('#power').textContent = power;
 }
 
-
-if(count == null){
-    localStorage.setItem('count' , '1')
+if (count == null) {
+    localStorage.setItem('count', '1');
 }
 
-image.addEventListener('click' , (e)=> {
-
+image.addEventListener('click', (e) => {
     let x = e.offsetX;
     let y = e.offsetY;
-
 
     navigator.vibrate(5);
 
     coins = localStorage.getItem('coins');
     power = localStorage.getItem('power');
-    
-    if(Number(power) > 0){
-        localStorage.setItem('coins' , `${Number(coins) + 1}`);
-        h1.textContent = `${(Number(coins) + 1).toLocaleString()}`;
-    
-        localStorage.setItem('power' , `${Number(power) - 1}`);
-        body.querySelector('#power').textContent = `${Number(power) - 1}`;
-    } 
 
-    if(x < 150 & y < 150){
+    if (Number(power) > 0) {
+        localStorage.setItem('coins', `${Number(coins) + 1}`);
+        h1.textContent = `${(Number(coins) + 1).toLocaleString()}`;
+
+        localStorage.setItem('power', `${Number(power) - 1}`);
+        body.querySelector('#power').textContent = `${Number(power) - 1}`;
+    }
+
+    if (x < 150 && y < 150) {
         image.style.transform = 'translate(-0.25rem, -0.25rem) skewY(-10deg) skewX(5deg)';
-    }
-    else if (x < 150 & y > 150){
+    } else if (x < 150 && y > 150) {
         image.style.transform = 'translate(-0.25rem, 0.25rem) skewY(-10deg) skewX(5deg)';
-    }
-    else if (x > 150 & y > 150){
+    } else if (x > 150 && y > 150) {
         image.style.transform = 'translate(0.25rem, 0.25rem) skewY(10deg) skewX(-5deg)';
-    }
-    else if (x > 150 & y < 150){
+    } else if (x > 150 && y < 150) {
         image.style.transform = 'translate(0.25rem, -0.25rem) skewY(10deg) skewX(-5deg)';
     }
 
-
-    setTimeout(()=>{
+    setTimeout(() => {
         image.style.transform = 'translate(0px, 0px)';
     }, 100);
 
     body.querySelector('.progress').style.width = `${(100 * power) / total}%`;
 });
 
-setInterval(()=> {
-    count = localStorage.getItem('count')
+setInterval(() => {
+    count = localStorage.getItem('count');
     power = localStorage.getItem('power');
-    if(Number(total) > power){
-        localStorage.setItem('power' , `${Number(power) + Number(count)}`);
+    if (Number(total) > power) {
+        localStorage.setItem('power', `${Number(power) + Number(count)}`);
         body.querySelector('#power').textContent = `${Number(power) + Number(count)}`;
         body.querySelector('.progress').style.width = `${(100 * power) / total}%`;
     }
